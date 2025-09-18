83fb3868d3df0c9a3a07df02cdc0dbe36e6e097c
edwin <12edwinho@gmail.com>
2025-09-18 17:10:17 +0800

    added kafka docker scripts and simple DAG script

---
+++ dags/kafka_stream.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from kafka import KafkaProducer
+
+def get_data():
+    # data_json is the raw json object from the API
+    data_json = requests.get("https://randomuser.me/api/")
+    # returns only the user info from the whole dict object
+    data_dict = data_json.json()
+    return data_dict['results'][0] # only returns the user info
+
+def format_data(data):
+    data = {
+        'first_name': data['name']['first'],
+        'last_name': data['name']['last'],
+        'email': data['email']
+    }
+    return data
+
+def stream_data():
+    data = get_data()
+    formatted_data = format_data(data)
+
+    # initializes a Kafka producer, which connects to Kafka broker running locally
+    # on port 9092 (defined in the docker-compose.yml file)
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+
+    # producer sends the formatte data to the kafka topic 'users_created'
+    producer.send('users_created', json.dumps(formatted_data).encode('utf-8'))
+
+stream_data()
